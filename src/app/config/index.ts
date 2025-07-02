6d21df48f21619d09ef87a2b370c529b300fcfc2
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-07-02 08:50:22 +0600

    feat: add dotenv configuration and environment variables setup

---
+++ src/app/config/index.ts
@@ -0,0 +1,27 @@
+import path from 'path'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: path.join(process.cwd(), '.env') })
+
+export default {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT || 5000,
+
+  strava: {
+    clientId: process.env.STRAVA_CLIENT_ID,
+    clientSecret: process.env.STRAVA_CLIENT_SECRET,
+    redirectUri: process.env.STRAVA_REDIRECT_URI,
+  },
+
+  firebase: {
+    config: process.env.FIREBASE_CONFIG, 
+  },
+
+  jwt: {
+    accessSecret: process.env.JWT_ACCESS_SECRET as string,
+    refreshSecret: process.env.JWT_REFRESH_SECRET as string,
+    accessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN || '1h',
+    refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7d',
+    resetPassExpiresIn: process.env.JWT_RESET_PASS_ACCESS_EXPIRES_IN || '5m',
+  },
+}
